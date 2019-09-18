45cd40cb700e1a10b6e66ae41078bcc512473af8
XQ <1379716219@qq.com>
2019-09-18 21:52:00 +0800

    HTMLController回来了

---
+++ src/main/java/com/xq/web/controller/HTMLController.java
@@ -0,0 +1,25 @@
+package com.xq.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 访问templates里的动态页面
+ *
+ * @author Mechrevo
+ * @version v1.0
+ * 2019/9/18 9:04
+ */
+@Controller
+public class HTMLController {
+    @RequestMapping("*.html")
+    public String welcome(HttpServletRequest request) {
+        /*
+        备用，返回null测试没问题，不明白为什么，如果返回null找不到页面404，使用下面的替代null
+        return request.getServletPath().substring(1).replace(".html", "");
+        */
+        return null;
+    }
+}
