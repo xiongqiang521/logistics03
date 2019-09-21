656b7091f109165fef2ed94ea43f90e8789e64b2
lixu <570397505@qq.com>
2019-09-21 13:53:06 +0800

    前端跳转

--- src/main/java/com/xq/web/controller/HTMLController.java
+++ src/main/java/com/xq/web/controller/HTMLController.java
@@ -3,8 +3,6 @@ package com.xq.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * 访问templates里的动态页面
  *
@@ -16,24 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 public class HTMLController {
     @RequestMapping({"*.html","*/*.html","*/*/*.html"})
     public String welcome() {
-        /*
-        备用，返回null测试没问题，不明白为什么，如果返回null找不到页面404，使用下面的替代null
-        return request.getServletPath().substring(1).replace(".html", "");
-        */
-        System.out.println(request.getServletPath().substring(1).replace(".html", ""));
-
-        return request.getServletPath().substring(1).replace(".html", "");
-
-    }
-    @RequestMapping("pages/*/*.html")
-    public String page(HttpServletRequest request) {
-        /*
-        备用，返回null测试没问题，不明白为什么，如果返回null找不到页面404，使用下面的替代null
-        return request.getServletPath().substring(1).replace(".html", "");
-        */
-        System.out.println(request.getServletPath().substring(1).replace(".html", ""));
-
-        return request.getServletPath().substring(1).replace(".html", "");
-
+        return null;
     }
 }
